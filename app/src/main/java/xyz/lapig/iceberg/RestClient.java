37923139cccdd49d7ab2f551d659a8fa9d0da85d
lapigr <lapigr@users.noreply.github.com>
2017-05-01 16:26:57 -0400

    Default to Lastfm and add getFullUrl

--- app/src/main/java/xyz/lapig/iceberg/RestClient.java
+++ app/src/main/java/xyz/lapig/iceberg/RestClient.java
@@ -9,12 +9,15 @@ import com.loopj.android.http.RequestParams;
  */
 
 public class RestClient {
-    private static final String BASE_URL = "https://lapig.xyz";
+    private static final String BASE_URL = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=lapigr&api_key=41ae03ee6e8fe7fc6be272cd0c3c51b6&format=json&limit=10";
 
     private static AsyncHttpClient client = new AsyncHttpClient();
 
     public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+    public static void getFullUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(url, params, responseHandler);
     }
 
     public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
