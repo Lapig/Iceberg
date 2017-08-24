e9276921462adb2fa26bae2a09e2cc5a28014980
lapig <pig@lapig.xyz>
2017-08-24 02:56:18 -0400

    make it still build, heck around with terrible globals abomination

--- lapig.iceberg/src/main/java/xyz/lapig/iceberg/Globals.java
+++ lapig.iceberg/src/main/java/xyz/lapig/iceberg/Globals.java
@@ -2,6 +2,8 @@ package xyz.lapig.iceberg;
 
 import android.app.Application;
 
+import java.util.HashMap;
+
 /**
  * Created by Lapig on 5/6/2017.
  */
@@ -10,7 +12,14 @@ public class Globals extends Application{
     private static String widgetText;
     private static String user;
     private static String limit;
+
+    private static HashMap<String, String> globalStore = new HashMap<String, String>();
+
     private static boolean updateNeeded=false;
+
+    public static void saveValue(String k, String v){
+        globalStore.put(k.toLowerCase(),v.toLowerCase());
+    }
 
     public static boolean isUpdateNeeded() {
         return updateNeeded;
